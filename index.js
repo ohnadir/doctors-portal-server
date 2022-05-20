870b9e44026d0f3dbf0f930cd4003f7fd25f35dc
ohnadir <qx.nadir@gmail.com>
2022-05-20 08:46:55 +0600

    get service api

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const express = require('express')
-const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oruy4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("doctors-portal").collection("service");
+    const bookingCollection = client.db("doctors-portal").collection("booking");
+
+    // get service API 
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+
+    
+  }
+  finally {
+    
+  }
+}
+run().catch(console.dir)
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`listening on port ${port}`)
 })
